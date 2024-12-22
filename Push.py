0f74f3dbf67dce2edfbdd9cc39b0bacb3de2c635
RandallGraida <carlosrandallgraida@gmail.com>
2024-12-22 16:13:17 +0800

    Provided some input in the code

--- Push.py
+++ Push.py
@@ -1,1 +1,5 @@
 print("Hello World")
+
+hi = 4
+hi_input = int(input("Write your desired number to be multiplied by four: "))
+print(hi_input * 18420184148898697857869578657867856888738712872166627678169645654786854086549068460854675476985483)
